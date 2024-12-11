5770f5b8ff725f2a1bf92584c960cca2b9691425
Nafis Mahmud Ayon <nafismahmudayon@gmail.com>
2024-12-11 19:02:42 +0600

    0.0.2

--- app/src/components/InfiniteScroll/InfiniteScroll.tsx
+++ app/src/components/InfiniteScroll/InfiniteScroll.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect } from "react";
-import "./scroll.css";
+// import "./scroll.css";
 
 interface ScrollerProps {
   className?: string;
--- lib/esm/components/InfiniteScroll/InfiniteScroll.d.ts
+++ lib/esm/components/InfiniteScroll/InfiniteScroll.d.ts
@@ -1,5 +1,4 @@
 import React from "react";
-import "./scroll.css";
 interface InfiniteScrollProps {
     className?: string;
     direction?: "left" | "right";
